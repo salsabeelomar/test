a4100467f943cc8f89362eb589aed6fc9f007818
salsabeelomar <omarsalsabeel65@gmail.com>
2026-04-12 00:54:40 +0300

    add test

---
+++ client/.eslintrc.cjs
@@ -0,0 +1,22 @@
+export default {
+  root: true,
+  env: { browser: true, es2020: true },
+  extends: [
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'plugin:react/jsx-runtime',
+    'plugin:react-hooks/recommended',
+  ],
+  ignorePatterns: ['dist'],
+  parserOptions: { ecmaVersion: 'latest', sourceType: 'module' },
+  settings: { react: { version: 'detect' } },
+  plugins: ['react-refresh'],
+  rules: {
+    'react/jsx-no-target-blank': 'off',
+    'react/display-name': 'off', 
+    'react-refresh/only-export-components': [
+      'warn',
+      { allowConstantExport: true },
+    ],
+  },
+};
